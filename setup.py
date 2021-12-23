141be58f1f7bf8485d33efbad2f37a8e45780fda
MXMarve <95297108+MXMarve@users.noreply.github.com>
2021-12-22 23:27:44 -0600

    Fixed setup.py not working correctly

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 import os
 def main():
     try:
-        import os
-        print("OS  is installed!")
-    except ImportError:
-        raise RuntimeError("Unable to automatically install OS. Please run pip install OS in terminal.")
-    try:
-        import random
-        print("Random is installed! ")
-    except ImportError:
-        raise RuntimeError("Random not installed! Installing Now... ")
         os.system("pip install random")
+        print("OS  is installed!")
+    except:
+        print("Unable to automatically install OS. Please run pip install os in terminal.")
     try:
-        import string
-        print("String is installed")
-    except ImportError:
-        raise RuntimeError("String is not installed! Installing Now... ")
+        os.system("pip install random")
+        print("Random is installed! ")
+    except:
+        print("Random Install Failed! Please do so manually (Run pip install random in your terminal)")
+    try:
         os.system("pip install string")
+        print("String is installed")
+    except:
+        print("String Install Failed! Please do so manually (Run pip install string in your terminal)")
     try:
-        from alive_progress import alive_bar
-        print("Alive Bar is installed!")
-    except ImportError:
-        raise RuntimeError("Alive Bar is not installed! Installing Now...")
         os.system("pip install alive_progress")
+        print("Alive Bar is installed!")
+    except:
+        print("Alive Progress Install Failed! Please do so manually (Run pip install alive_progress in your terminal)")
 def compute():
     for i in range(1):
         ...
